36f4829d02ad75ca710f674619b8b2da7a86bad5
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-04-07 15:38:33 -0300

    add genre findAll

---
+++ src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -0,0 +1,22 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.dto.GenreDTO;
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.repositories.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class GenreService {
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @Transactional(readOnly = true)
+    public List<GenreDTO> findAll() {
+        List<Genre> genres = genreRepository.findAll();
+        return genres.stream().map(GenreDTO::new).toList();
+    }
+}
